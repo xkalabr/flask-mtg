d7016e567edd1d76252520e7cdeba0e8def90dc3
Charles Schultz <cschultz@cox.net>
2018-05-12 11:14:03 -0700

    Added card details API

--- mtgdb/index.py
+++ mtgdb/index.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
-import mysql.connector
+from sqlalchemy import create_engine
 app = Flask(__name__)
-cnx = mysql.connector.connect(user='mtgadmin', password='blackL0tus',host='127.0.0.1', database='mtg')
+engine = create_engine('mysql+mysqlconnector://mtgadmin:blackL0tus@localhost/mtg')
 
 IMAGE_BASE_PATH = '../mtg/images/'
 
@@ -17,10 +17,33 @@ def show_image(filename):
 @app.route("/cardsets")
 def list_sets():
 	retval = []
-	cursor = cnx.cursor()
-	cursor.execute('select code,name from cardsets order by reldate desc')
-	result = cursor.fetchall()
+	result = engine.execute('select code,name from cardsets order by reldate desc')
 	for row in result:
 		retval.append({'code': row[0], 'name': row[1]})
 	return jsonify(retval)
 
+@app.route("/card/<string:cid>")
+def show_card(cid):
+	retval = {'cid': cid}
+	sql = (
+		'select c.name, manacost, c.type, rarity, ctext, ftext, p, t, l, imgpath, setcode, '
+		'cardsets.name from cards as c,cardsets where cid=\'' + cid + '\' and code=setcode')
+	result = engine.execute(sql)
+	for row in result:
+		retval['name'] = row[0]
+		retval['manacost'] = row[1]
+		retval['type'] = row[2]
+		retval['rarity'] = row[3]
+		retval['ctext'] = row[4]
+		retval['ftext'] = row[5]
+		retval['p'] = row[6]
+		retval['t'] = row[7]
+		retval['l'] = row[8]
+		if row[9] is not None:
+			ipath = row[9].split('/')
+			retval['imgpath'] = ipath[3]
+		else:
+			retval['imgpath'] =  ''
+		retval['setcode'] = row[10]
+		retval['setname'] = row[11]
+	return jsonify(retval)
