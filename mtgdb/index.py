165263c082995f892be32cde5e6dbd3c321a6960
Charles Schultz <cschultz@cox.net>
2018-05-13 12:30:43 -0700

    Added inventory APIs

--- mtgdb/index.py
+++ mtgdb/index.py
@@ -66,3 +66,49 @@ def card_search(term):
 		entry['setname'] = row[4]
 		retval.append(entry)
 	return jsonify(retval)
+
+@app.route("/cardinventory/<string:cid>")
+def card_inventory(cid):
+	retval = {'stock': [], 'foil': [], 'deck': []}
+	sql = (
+		'select i.id,name,price,condval,indeck,isfoil from inventory as i,cardcondition '
+		'as cc,cards as c where i.cid=\'' + cid + '\' and cond=cc.id and i.cid=c.cid '
+		'order by cc.id'
+	)
+	result = engine.execute(sql)
+	for row in result:
+		entry = {}
+		entry['id'] = row[0]
+		entry['name'] = row[1]
+		entry['price'] = row[2]
+		entry['cond'] = row[3]
+		indeck = row[4]
+		isfoil = row[5]
+		if indeck:
+			retval['deck'].append(entry)
+		elif isfoil:
+			retval['foil'].append(entry)
+		else:
+			retval['stock'].append(entry)
+	return jsonify(retval)
+
+@app.route("/setinventory/<string:id>")
+def set_inventory(id):
+	retval = []
+	sql = (
+		'select count(*),ANY_VALUE(c.cid),c.name,ANY_VALUE(c.type),ANY_VALUE(rarity),'
+		'ANY_VALUE(price) from inventory as i,cardsets as cs,cards as c where '
+		'cs.id=\'' + id + '\' and setcode=code and i.cid=c.cid group by c.name '
+		'order by c.name'
+	)
+	result = engine.execute(sql)
+	for row in result:
+		entry = {}
+		entry['num'] = row[0]
+		entry['cid'] = row[1]
+		entry['name'] = row[2]
+		entry['type'] = row[3]
+		entry['rarity'] = row[4]
+		entry['price'] = row[5]
+		retval.append(entry)
+	return jsonify(retval)
